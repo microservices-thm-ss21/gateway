1fe89d617e55654550e875b3a66eeead1de27ace
nhrt <hartingerniklas@gmail.com>
2021-07-07 14:25:39 +0200

    First workig routes

--- src/main/kotlin/de/thm/mni/microservices/gruppe6/gateway/GatewayApplication.kt
+++ src/main/kotlin/de/thm/mni/microservices/gruppe6/gateway/GatewayApplication.kt
@@ -3,32 +3,52 @@ package de.thm.mni.microservices.gruppe6.gateway
 import de.thm.mni.microservices.gruppe6.gateway.endpoints.IssueEndpoint
 import de.thm.mni.microservices.gruppe6.gateway.endpoints.ProjectEndpoint
 import de.thm.mni.microservices.gruppe6.gateway.endpoints.UserEndpoint
+import de.thm.mni.microservices.gruppe6.gateway.model.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.context.annotation.Bean
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
-import org.springframework.web.bind.annotation.CrossOrigin
+import reactor.core.publisher.Mono
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 
 @SpringBootApplication
 class GatewayApplication {
+    val user : User = User(UUID.fromString("a443ffd0-f7a8-44f6-8ad3-87acd1e91042"), "Peter_Zwegat", "Peter", "Zwegat", "peter.zwegat@mni.thm.de", LocalDate.now(), LocalDateTime.now(), "normal", null)
+
     @Bean
-    @CrossOrigin
     fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator {
         return builder.routes {
+            // project service
             route {
-                path("/${ProjectEndpoint.BASE.url}/**")
+                path("/${ProjectEndpoint.BASE.url}/user")
+                filters { rewritePath("user","users/${user.id}") }
                 uri(ProjectEndpoint.SERVICE.url)
             }
+            route {
+                path("/${ProjectEndpoint.BASE.url}/projects/*/members")
+                filters {
+
+                    rewritePath("*","users/${user.id}")
+
+                }
+                uri(ProjectEndpoint.SERVICE.url)
+            }
+            // issue service
             route {
                 path("/${IssueEndpoint.BASE.url}/**")
                 uri(IssueEndpoint.SERVICE.url)
             }
+            // user service
             route {
                 path("/${UserEndpoint.BASE.url}/**")
                 uri(UserEndpoint.SERVICE.url)
             }
+            // news service
         }
     }
 }
