305ac1b308510bcaaf07abc41dbbbfd05f7ed5f5
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-07-21 22:20:36 +0200

    dependency fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,6 @@ repositories {
     mavenCentral()
 }
 
-extra["springBootAdminVersion"] = "2.3.1"
-
 buildscript {
     dependencies {
         classpath("com.avast.gradle:gradle-docker-compose-plugin:0.14.2")
@@ -40,25 +38,22 @@ dependencyManagement {
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("io.micrometer:micrometer-registry-influx:1.7.1")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-actuator") {
+        exclude("org.springframework.boot.autoconfigure")
+    }
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("de.thm.mni.microservices.gruppe6:service-lib:0.0.1-SNAPSHOT")
+    implementation("de.thm.mni.microservices.gruppe6:service-lib:0.0.1-SNAPSHOT") {
+        exclude("org.springframework.boot.autoconfigure")
+    }
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.security:spring-security-test")
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
-    }
 }
 
 tasks.withType<KotlinCompile> {
