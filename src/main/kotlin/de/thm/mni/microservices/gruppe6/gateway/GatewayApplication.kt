53070425c6ecaca92e8316dbd0448088917981d5
Jannis Weber <jannis.weber@mni.thm.de>
2021-07-21 15:37:39 +0200

    fix error on startup due to embedded database

--- src/main/kotlin/de/thm/mni/microservices/gruppe6/gateway/GatewayApplication.kt
+++ src/main/kotlin/de/thm/mni/microservices/gruppe6/gateway/GatewayApplication.kt
@@ -1,36 +1,19 @@
 package de.thm.mni.microservices.gruppe6.gateway
 
-import de.thm.mni.microservices.gruppe6.gateway.endpoints.IssueEndpoint
-import de.thm.mni.microservices.gruppe6.gateway.endpoints.ProjectEndpoint
-import de.thm.mni.microservices.gruppe6.gateway.endpoints.UserEndpoint
-import de.thm.mni.microservices.gruppe6.gateway.filter.UserIsMemberFilter
-import de.thm.mni.microservices.gruppe6.gateway.message.GatewayProjectDTO
-import de.thm.mni.microservices.gruppe6.lib.classes.projectService.ProjectDTO
-import de.thm.mni.microservices.gruppe6.lib.classes.userService.User
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.cloud.gateway.filter.GatewayFilter
-import org.springframework.cloud.gateway.route.RouteLocator
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.cloud.gateway.route.builder.filters
-import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
-import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.filter.HiddenHttpMethodFilter
-import reactor.core.publisher.Mono
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.util.*
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class, R2dbcAutoConfiguration::class])
 class GatewayApplication {
+
 
     /**
      * Override doFilterInternal to make HTTP-Methods delete and put work
